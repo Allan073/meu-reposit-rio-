061ddc5e7bc69df3bcb8ecf21447607d3356dfc1
Allan073 <allanvictorv7@gmail.com>
2024-11-12 10:40:30 -0300

    Create Quadrado.java

---
+++ LP II: Ativ. 2/2-Herança/Quadrado.java
@@ -0,0 +1,13 @@
+public class Quadrado {
+
+    private int lado;
+
+    public Quadrado(int l){
+        this.lado = l;
+    }
+
+    public int getArea(){
+        return lado*lado;
+    }
+    
+}
